d282655fee6817f585bc1f757a1984ad3fd3b507
Sydney Renee <sydney@solace.ofharmony.ai>
2026-08-25 03:53:07 -0700

    refactor(itertools): align ImplMacros as module ledger and consolidate iproduct in ConsTuplesImpl

--- src/commonMain/kotlin/io/github/kotlinmania/itertools/ConsTuplesImpl.kt
+++ src/commonMain/kotlin/io/github/kotlinmania/itertools/ConsTuplesImpl.kt
@@ -35,3 +35,51 @@ fun <A, B, C> consTuples(iterable: Iterable<Pair<Pair<A, B>, C>>): ConsTuples<A,
  */
 fun <A, B, C> consTuples(iterator: Iterator<Pair<Pair<A, B>, C>>): ConsTuples<A, B, C> =
     ConsTuples(iterator)
+
+/**
+ * Cartesian product of two iterables.
+ */
+fun <A, B> iproduct(a: Iterable<A>, b: Iterable<B>): Iterator<Pair<A, B>> =
+    sequence {
+        for (x in a) {
+            for (y in b) {
+                yield(Pair(x, y))
+            }
+        }
+    }.iterator()
+
+/**
+ * Cartesian product of three iterables.
+ */
+fun <A, B, C> iproduct(a: Iterable<A>, b: Iterable<B>, c: Iterable<C>): Iterator<Triple<A, B, C>> =
+    sequence {
+        for (x in a) {
+            for (y in b) {
+                for (z in c) {
+                    yield(Triple(x, y, z))
+                }
+            }
+        }
+    }.iterator()
+
+/**
+ * Cartesian product of four iterables.
+ */
+fun <A, B, C, D> iproduct(
+    a: Iterable<A>,
+    b: Iterable<B>,
+    c: Iterable<C>,
+    d: Iterable<D>,
+): Iterator<List<Any?>> =
+    sequence {
+        for (w in a) {
+            for (x in b) {
+                for (y in c) {
+                    for (z in d) {
+                        yield(listOf(w, x, y, z))
+                    }
+                }
+            }
+        }
+    }.iterator()
+
